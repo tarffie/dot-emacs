e6df61a09e2c1e1c355b71aca24045c0d316dca4
Emilia Alves <enbrakaffe@gmail.com>
2024-03-18 10:13:04 -0300

    added my emacs dir folder and snippets

---
+++ .emacs.d/elpa/tree-sitter-langs-20240210.2155/tree-sitter-langs-pkg.el
@@ -0,0 +1,15 @@
+(define-package "tree-sitter-langs" "20240210.2155" "Grammar bundle for tree-sitter"
+  '((emacs "25.1")
+    (tree-sitter "0.15.0"))
+  :commit "5c24a092739f1136c012e71ea88b7af2e2379295" :authors
+  '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com"))
+  :maintainers
+  '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com"))
+  :maintainer
+  '("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com")
+  :keywords
+  '("languages" "tools" "parsers" "tree-sitter")
+  :url "https://github.com/emacs-tree-sitter/tree-sitter-langs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
